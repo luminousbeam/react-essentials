c2b2d29bca454133b992165868850f4159cd1d45
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-16 14:00:30 -0400

    create function component using props

--- src/index.js
+++ src/index.js
@@ -8,36 +8,43 @@ let skiData = {
   goal: 100
 }
 
-class SkiDayCounter extends React.Component{
-  getPercent = decimal => {
-    return decimal * 100 + '%'
-  }
-
-  calcGoalProgress = (total, goal) => {
-    return this.getPercent(total/goal)
-  }
-
-  render(){
-    const { total, powder, backcountry, goal } = this.props
-    return (
-      <section>
-        <h1>Ski Days</h1>
-        <div>
-          <p>Total Days: {total}</p>
-        </div>
-        <div>
-          <p>Powder Days: {powder}</p>
-        </div>
-        <div>
-          <p>Backcountry Days: {backcountry}</p>
-        </div>
-        <div>
-          <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
-        </div>
-      </section>
-    )
-  }
+const SkiDayCounter = (props) => {
+  return (
+    <section>
+      <h1>Ski Days</h1>
+      <div>
+        <p>Total Days: {props.total}</p>
+      </div>
+      <div>
+        <p>Powder Days: {props.powder}</p>
+      </div>
+      <div>
+        <p>Backcountry Days: {props.backcountry}</p>
+      </div>
+      <div>
+        <p>Goal Progress: {props.goal}</p>
+      </div>
+    </section>
+  )
 }
+
+// this.calcGoalProgress(total, goal)
+// class SkiDayCounter extends React.Component{
+//   getPercent = decimal => {
+//     return decimal * 100 + '%'
+//   }
+//
+//   calcGoalProgress = (total, goal) => {
+//     return this.getPercent(total/goal)
+//   }
+//
+//   render(){
+//     const { total, powder, backcountry, goal } = this.props
+//     return (
+//
+//     )
+//   }
+// }
 
 render(
   <SkiDayCounter
