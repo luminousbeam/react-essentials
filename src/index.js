81ea4e36647283b96218ad978c524c20649e8bdc
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-16 14:17:18 -0400

    compose components: parent Library, child Book

--- src/index.js
+++ src/index.js
@@ -1,46 +1,29 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
-}
-
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total/goal)
-}
-
-const SkiDayCounter = ({ total, powder, backcountry, goal }) => {
+const Book = ({ title, author, pages }) => {
   return (
     <section>
-      <h1>Ski Days</h1>
-      <div>
-        <p>Total Days: {total}</p>
-      </div>
-      <div>
-        <p>Powder Days: {powder}</p>
-      </div>
-      <div>
-        <p>Backcountry Days: {backcountry}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-      </div>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages} pages</p>
     </section>
   )
 }
 
+const Library = () => {
+  return (
+    <div>
+      <h1>Welcome to the Library</h1>
+      <Book title="Little Women" author="Louissa May Alcott" pages={759} />
+      <Book title="The Sun Also Rises" author="Zadie Smith" pages={480} />
+      <Book title="White Teeth" author="Louissa May Alcott" pages={759} />
+      <Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304} />
+    </div>
+  )
+}
+
 render(
-  <SkiDayCounter
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal} />,
+  <Library />,
   document.getElementById('root')
 )
