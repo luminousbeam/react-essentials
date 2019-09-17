142bba6448ad2ba6748441ed048b10fccf474abf
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-16 22:14:03 -0400

    fetching data

--- src/index.js
+++ src/index.js
@@ -33,7 +33,20 @@ class Library extends Component {
   state = {
     open: true,
     freeBookMark: false,
-    hiring: false
+    hiring: false,
+    data: [],
+    loading: false
+  }
+
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+          .then(data => data.json())
+          .then(data => this.setState({data, loading: false}))
+  }
+
+  componentDidUpdate() {
+    console.log("The component just updated!")
   }
 
   toggleOpenClosed = () => {
@@ -48,6 +61,20 @@ class Library extends Component {
     return (
       <div>
         {this.state.hiring ? <Hiring /> : <NotHiring />}
+        {this.state.loading
+          ? "loading..."
+          : <div>
+              {this.state.data.map(product => {
+                return (
+                  <div key={product.id}>
+                    <h3>Library Product of the Week!</h3>
+                    <h4>{product.name}</h4>
+                    <img alt={product.name} src={product.image} height={100} />
+                  </div>
+                )
+              })}
+            </div>
+        }
         <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
         <button onClick={this.toggleOpenClosed}>Change</button>
         {books.map(
